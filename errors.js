480cc92d8cc95895a75a723bac1275efa6ac151b
Kashima <phoenixreid03@gmail.com>
2020-09-29 18:08:21 +1000

    Test

--- errors.js
+++ errors.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const Discord = require("discord.js");
 const fs = require("fs");
 let config = require("../botconfig.json");
@@ -50,57 +50,4 @@ module.exports.noReason = (channel) => {
         .setColor(config.red);
 
     channel.send(embed).then(m => m.delete(5000));
-=======
-const Discord = require("discord.js");
-const fs = require("fs");
-let config = require("../botconfig.json");
-
-module.exports.noPerms = (message, perm) => {
-    let embed = new Discord.MessageEmbed
-        .setAuthor(message.author.username)
-        .setTitle("Insufficient Permission")
-        .setColor(config.red)
-        .addField("Permission needed", perm);
-
-    message.channel.send(embed).then(m => m.delete(5000));
-}
-
-module.exports.equalPerms = (message, user, perms) => {
-
-    let embed = new Discord.MessageEmbed
-        .setAuthor(message.author.username)
-        .setColor(config.red)
-        .setTitle("Error")
-        .addField(`${user} has perms`, perms);
-
-    message.channel.send(embed).then(m => m.delete(5000));
-
-}
-
-module.exports.botuser = (message) => {
-    let embed = new Discord.MessageEmbed
-        .setTitle("Error")
-        .setDescription("You cannot ban a bot.")
-        .setColor(config.red);
-
-    message.channel.send(embed).then(m => m.delete(5000));
-}
-
-module.exports.cantfindUser = (channel) => {
-    let embed = new Discord.MessageEmbed
-        .setTitle("Error")
-        .setDescription("Could not find that user.")
-        .setColor(config.red);
-
-    channel.send(embed).then(m => m.delete(5000));
-}
-
-module.exports.noReason = (channel) => {
-    let embed = new Discord.MessageEmbed
-        .setTitle("Error")
-        .setDescription("Please supply a reason.")
-        .setColor(config.red);
-
-    channel.send(embed).then(m => m.delete(5000));
->>>>>>> 911471a31e69d42a8e7715c4659360a597d8e9b8
 }
--- types/month.js
+++ types/month.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { ArgumentType } = require('discord.js-commando');
 const months = require('../assets/json/month');
 
@@ -20,26 +19,3 @@ module.exports = class MonthArgumentType extends ArgumentType {
 		return months.indexOf(value.toLowerCase()) + 1;
 	}
 };
-=======
-const { ArgumentType } = require('discord.js-commando');
-const months = require('../assets/json/month');
-
-module.exports = class MonthArgumentType extends ArgumentType {
-	constructor(client) {
-		super(client, 'month');
-	}
-
-	validate(value) {
-		const num = Number.parseInt(value, 10);
-		if (num > 0 && num < 13) return true;
-		if (months.includes(value.toLowerCase())) return true;
-		return false;
-	}
-
-	parse(value) {
-		const num = Number.parseInt(value, 10);
-		if (!Number.isNaN(num)) return num;
-		return months.indexOf(value.toLowerCase()) + 1;
-	}
-};
->>>>>>> 911471a31e69d42a8e7715c4659360a597d8e9b8
