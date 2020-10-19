af169db8df55a4e199f81cb6995b9211dea3d747
KazutoKashima <phoenixreid03@gmail.com>
2020-10-19 12:04:44 +1100

    redone bot permissions

---
+++ commands/utils/testhook.js
@@ -0,0 +1,34 @@
+// JavaScript source code
+const { Command } = require('discord.js-commando');
+const MessageEmbed = require('discord.js');
+const devID = '286957313489764353';
+
+module.exports = class TestWebhookCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'testhook',
+            group: 'utils',
+            memberName: 'testhook',
+            description: 'A WebHook tester',
+        });
+    }
+    run(msg) {
+        if (msg.author.id != devID) return;
+
+        const hookEmbed1 = MessageEmbed()
+            .setTitle("Test Webhook 1")
+            .setColor("#0099ff")
+        try {
+            const webhooks = msg.channel.fetchWebhooks();
+            const webhook = webhooks.first();
+
+            await webhook.send('Webhook test', {
+                username: 'Mika\'s test webhook',
+                avatarURL: '',
+                embeds: [hookEmbed1],
+            });
+        } catch (e) {
+            console.error(e.stack);
+        }
+    }
+};
